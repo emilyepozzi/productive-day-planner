3d8d1744fce4fdd4fd7667db4396364c38abb7fd
Emily Pozzi <epozzi5@yahoo.com>
2021-03-21 16:15:37 -0500

    figuring out jquery for local storage

--- script.js
+++ script.js
@@ -16,25 +16,47 @@
 //     console.log(displayTime);
 // }, 1000)
 
-
 //mood of the day, saving to local storage
 
-
-
 // Get the button, and when the user clicks on it, execute myFunction
-var taskBtn = document.querySelector(".btn").addEventListener("click", function() {
-var taskEl = $('.text-area');
-localStorage.setItem(".text-area", taskEl.val());
-console.log(taskEl.val());
+var taskBtn = document
+  .querySelector(".btn")
+  .addEventListener("click", function () {
+    var taskEl = $(".text-area");
+    var time = this.value;
+    var task = taskEl.val();
+    localStorage.setItem(time, task);
+    console.log(taskEl.val());
 
-// for each to save individual text area boxes
-// taskEl.forEach(taskEl);
+    // for each to save individual text area boxes
+    // taskEl.forEach(taskEl);
+  });
 
-});
-
-var storedValue = localStorage.getItem(".text-area");
+// var storedValue = localStorage.getItem(".text-area");
 
 //grab the data in local storage and fill in the textarea values
-$('text-area').each(function() {
-    $(this).val(localStorage.getItem($(this).attr('text-area')));
-});
+$("#8am-text").val(localStorage.getItem("8am"));
+$("#9am-text").val(localStorage.getItem("9am"));
+$("#10am-text").val(localStorage.getItem("10am"));
+$("#11am-text").val(localStorage.getItem("11am"));
+$("#12pm-text").val(localStorage.getItem("12pm"));
+$("#1pm-text").val(localStorage.getItem("1pm"));
+$("#2pm-text").val(localStorage.getItem("2pm"));
+$("#3pm-text").val(localStorage.getItem("3pm"));
+$("#4pm-text").val(localStorage.getItem("4pm"));
+$("#5pm-text").val(localStorage.getItem("5pm"));
+$("#6pm-text").val(localStorage.getItem("6pm"));
+$("#7pm-text").val(localStorage.getItem("7pm"));
+$("#8pm-text").val(localStorage.getItem("8pm"));
+$("#9pm-text").val(localStorage.getItem("9pm"));
+$("#10pm-text").val(localStorage.getItem("10pm"));
+$("#11pm-text").val(localStorage.getItem("11pm"));
+$("#12am-text").val(localStorage.getItem("12am"));
+$("#1am-text").val(localStorage.getItem("1am"));
+$("#2am-text").val(localStorage.getItem("2am"));
+$("#3am-text").val(localStorage.getItem("3am"));
+$("#4am-text").val(localStorage.getItem("4am"));
+$("#5am-text").val(localStorage.getItem("5am"));
+$("#6am-text").val(localStorage.getItem("6am"));
+$("#7am-text").val(localStorage.getItem("7am"));
+
