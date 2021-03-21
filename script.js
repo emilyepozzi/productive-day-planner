4d6619de876b318ac6fed0c9f73c71b230fd2528
Emily Pozzi <epozzi5@yahoo.com>
2021-03-21 12:40:49 -0500

    trying to find out how to save to local storage

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const currentDate = moment().format("MMMM Do, YYYY");
-const displayDate = document.getElementById("currentDay");
-const currentHour = moment().format("HH:mm");
+// const currentDate = moment().format("MMMM Do, YYYY");
+// const displayDate = document.getElementById("currentDay");
+// const currentHour = moment().format("HH:mm");
 
-setInterval (function constDate() {
-    const showDate = document.getElementById("currentDay")
-    const nowDate = moment().format("MMMM Do, YYYY")
-    showDate.innerHTML = nowDate;
+// setInterval (function constDate() {
+//     const showDate = document.getElementById("currentDay")
+//     const nowDate = moment().format("MMMM Do, YYYY")
+//     showDate.innerHTML = nowDate;
 
-})
+// })
 
-setInterval (function constTime() {
-    const displayTime = document.getElementById("currentTime")
-    const nowTime = moment().format("h:mm:ss A")
-    displayTime.innerHTML = nowTime;
-    console.log(displayTime);
-}, 1000)
+// setInterval (function constTime() {
+//     const displayTime = document.getElementById("currentTime")
+//     const nowTime = moment().format("h:mm:ss A")
+//     displayTime.innerHTML = nowTime;
+//     console.log(displayTime);
+// }, 1000)
+
+
+//mood of the day, saving to local storage
+
+
+
+// Get the button, and when the user clicks on it, execute myFunction
+var taskBtn = document.querySelector(".btn").addEventListener("click", function() {
+var taskEl = $("#text-area");
+localStorage.setItem("text-area", taskEl.val());
+console.log(taskEl.val());
+});
+
+var storedValue = localStorage.getItem("text-area");
 
