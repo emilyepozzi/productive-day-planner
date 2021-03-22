74da36aabc5da8f60f5c5ff7d12e003a2b27e025
Emily Pozzi <epozzi5@yahoo.com>
2021-03-21 19:35:28 -0500

    finished changes to rows and style

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-// const currentDate = moment().format("MMMM Do, YYYY");
-// const displayDate = document.getElementById("currentDay");
-// const currentHour = moment().format("HH:mm");
+const currentDate = moment().format("MMMM Do, YYYY");
+const displayDate = document.getElementById("currentDay");
+const currentHour = moment().format("HH:mm");
 
-// setInterval (function constDate() {
-//     const showDate = document.getElementById("currentDay")
-//     const nowDate = moment().format("MMMM Do, YYYY")
-//     showDate.innerHTML = nowDate;
+setInterval (function constDate() {
+    const showDate = document.getElementById("currentDay")
+    const nowDate = moment().format("MMMM Do, YYYY")
+    showDate.innerHTML = nowDate;
 
-// })
+})
 
-// setInterval (function constTime() {
-//     const displayTime = document.getElementById("currentTime")
-//     const nowTime = moment().format("h:mm:ss A")
-//     displayTime.innerHTML = nowTime;
-//     console.log(displayTime);
-// }, 1000)
+setInterval (function constTime() {
+    const displayTime = document.getElementById("currentTime")
+    const nowTime = moment().format("h:mm:ss A")
+    displayTime.innerHTML = nowTime;
+    console.log(displayTime);
+}, 1000)
 
-//mood of the day, saving to local storage
 
 // Get the button, and when the user clicks on it, execute myFunction
 var taskBtn = document
